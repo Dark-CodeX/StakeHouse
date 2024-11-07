d06b870064c1971e4482bbd1b4cd8c82fa8cdacc
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2024-11-08 02:17:19 +0530

    using `user` class

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
-import os
-import random
+import user.user as stakehouse_user
+import games.limbo as stakehouse_limbo
 
 FILE_PATH = "./user.cnf"
 
-def create_user():
-    name = input("Enter your name: ")
-    balance = 500
-    wager = 0
-    srand = random.randint(1000000, 9999999) * random.randint(1000000, 9999999)
-    save = open(FILE_PATH, "w")
-    save.write(name + "\n" + str(balance) + "\n" + str(wager) + "\n" + str(srand) + "\n")
-    save.close()
-
 if __name__ == "__main__":
-    if os.path.exists(FILE_PATH) == False:
-        create_user()
-    else:
-        user = open(FILE_PATH, "r")
-        user_content = user.readlines()
-        user.close()
-        print("Welcome to StakeHouse, dear", user_content[0], end="")
-        print("Your current points are:", user_content[1], end="")
+    user = stakehouse_user.user()
+    user.open(FILE_PATH)
+    print("Welcome to StakeHouse, dear", user.name, end="")
+    while True:
+        print("Your current points are:", user.balance)
         print("Select the game you wanna play?")
         print("\t1. Mines")
-        print("\t2. ")
-        option = input("Your choice? ")
+        print("\t2. Limbo")
+        print("\t0: Exit")
+        game_option = int(input("Your choice? "))
+        if game_option == 1:
+            pass
+        elif game_option == 2:
+            stakehouse_limbo.limbo(user)
+        elif game_option == 0:
+            break
+        else:
+            print("Invalid choice")
+                
